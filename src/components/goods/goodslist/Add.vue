82f55de03a67bcdc9db565c04667d2ea4706564c
09o <igsober@gmail.com>
2020-09-02 13:13:21 +0800

    添加商品-基本信息

--- src/components/goods/goodslist/Add.vue
+++ src/components/goods/goodslist/Add.vue
@@ -8,13 +8,32 @@
       <el-step title="商品图片"></el-step>
       <el-step title="商品内容"></el-step>
     </el-steps>
-    <el-tabs v-model="activeTab" :tab-position="tabPosition" style="height: 200px; margin: 20px 0;">
-      <el-tab-pane name="0" label="基本信息">基本信息</el-tab-pane>
-      <el-tab-pane name="1" label="商品参数">商品参数</el-tab-pane>
-      <el-tab-pane name="2" label="商品属性">商品属性</el-tab-pane>
-      <el-tab-pane name="3" label="商品图片">商品图片</el-tab-pane>
-      <el-tab-pane name="4" label="商品内容">商品内容</el-tab-pane>
-    </el-tabs>
+    <el-form label-position="left" label-width="80px" :model="formLabelAlign">
+      <el-tabs v-model="activeTab" tab-position="right" style="margin: 20px 0;">
+        <el-tab-pane name="0" label="基本信息">
+          <el-form-item label="商品名称">
+            <el-input v-model="goodsInfo.goods_name"></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格">
+            <el-input v-model="goodsInfo.goods_price"></el-input>
+          </el-form-item>
+          <el-form-item label="商品重量">
+            <el-input v-model="goodsInfo.goods_weight"></el-input>
+          </el-form-item>
+          <el-form-item label="商品数量">
+            <el-input v-model="goodsInfo.goods_number"></el-input>
+          </el-form-item>
+          <el-form-item label="商品分类">
+            <el-input v-model="goodsInfo.goods_name"></el-input>
+          </el-form-item>
+          <el-button type="primary" @click="next">下一步</el-button>
+        </el-tab-pane>
+        <el-tab-pane name="1" label="商品参数">商品参数</el-tab-pane>
+        <el-tab-pane name="2" label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane name="3" label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane name="4" label="商品内容">商品内容</el-tab-pane>
+      </el-tabs>
+    </el-form>
   </el-card>
 </template>
 
@@ -23,12 +42,25 @@ export default {
   data() {
     return {
       activeTab: 0,
-      tabPosition: "right",
+      goodsInfo: {
+        goods_name: '',
+        goods_cat: '',
+        goods_price: '',
+        goods_number: '',
+        goods_weight: '',
+        goods_introduce: '',
+        pics: '',
+        attrs: '',
+      },
     };
   },
   methods: {
     goBack() {
       this.$router.push({ name: "goods" });
+    },
+    next() {
+      +this.activeTab++;
+      this.activeTab = this.activeTab.toString();
     },
   },
 };
